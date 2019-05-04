3ca3d760a10ef836d3019c08e335c4620bfe4394
Unknown <lordh@wp.pl>
2019-05-04 18:48:56 +0200

    Basic informations

---
+++ src/components/Experience.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <section>
+      <h1>{{ msg }}</h1>
+      <p v-for="(item, index) in experience" :key="index"><b>{{ index + 1 }})</b> {{ item }}</p>
+    </section>
+    <section>
+      <h2>{{ testingExperienceTitle }}</h2>
+      <p v-for="(item, index) in testingExperience" :key="index"><b>{{ index + 1 }})</b> {{ item }}</p>
+    </section>
+    <section>
+      <h3>LinkedIn</h3>
+      <a href="https://www.linkedin.com/in/karolina-bogacka-289292163/" target="_blank" rel="noopener">Link</a>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      experience: [
+        "Własna działalność gospodarcza - SPEDIA K.B. Karolina Bogacka, od 2011 r.",
+        "Współwłaściciel w Greysoft Sp. z o. o., od 2017 r.",
+        "Instruktor nauki jazdy kat. B, B1",
+      ],
+      testingExperienceTitle: "Doświadczenie w testowaniu oprogramowania",
+      testingExperience: [
+        "Szkolenie indywidualne - tester manualny - teoria i praktyka - Waldemar Szafraniec",
+        "Szkolenie przygotowujące do egzaminu ISTQB FL wraz z ćwiczeniami praktycznymi - Testuj.pl",
+        "Szkolenie teoretyczne - Wstęp do pracy na stanowisku Tester Manualny - Anna Pieciulko-Czyrko",
+        "Certyfikat ISTQB FL"
+      ]
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+section {
+  margin-bottom: 50px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png"><br/>
+    <h1><i><b>Karolina Bogacka</b></i></h1><br/>
+    <b>Miasto:</b> Gdańsk<br/>
+    <b>Email:</b> kbogacka1@wp.pl<br/>
+    <i><b>Telefon:</b> 535 365 341</i><br/>
+    <p>Poszukuję pierwszej pracy/stażu (także bez płatnego)/praktyk, na stanowisku Junior Software Manual Tester</p>
+
+    <HelloWorld msg="Doświadczenie zawodowe"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Experience from '@/components/Experience.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld: Experience
   }
 }
 </script>
